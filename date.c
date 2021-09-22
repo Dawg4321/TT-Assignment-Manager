68c5038f7705a814f397f7216d627dad60a8d24d
Dawg4321 <ryan.wiebe@outlook.com>
2021-09-22 12:15:41 +0100

    Improved CalcDueDays & LeapYear Comments

--- date.c
+++ date.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-// Global Variables
+// *** Global Variables ***
 int g_month_arr[2][12] = {{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, // arr storing num of days in each month of year
                           {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}}; // [0] = leapless year, [1] = leap year
-// Structs
+// *** Structs ***
 typedef struct Date{ // struct to store a date
     int day,month,year;
 }Date;
@@ -15,14 +15,27 @@ typedef struct Assignment{ // struct to store data relating to each assignment d
     char z[255];
 }Assignment;
 
-// Functions
+// *** Functions ***
 
+// ~~~ LeapYear ~~~~
+// Determines whether a year is a leap year. 
+// Pass year to test as int.
+// If LeapYear, return 1
+// If !Leapyear, return 0 
+// ~~~~~~~~~~~~~~~~~
 int LeapYear(int y){
     if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0))
         return 1;
     return 0;
 }
 
+// ~~~ CalcDueDays ~~~
+// Determines number of days inbetween two dates. Return variable: due_days
+// E.g. year2 = 21/12/2021 & year1 = 22/09/21. due_days = 120
+// Other Cases:
+// If year1 > year2, due_days = negative value
+// If year1 == year2, due_days = 0
+// ~~~~~~~~~~~~~~~~~~~
 int CalcDueDays(Date year1, Date year2){
     
     int due_days = 0;
@@ -48,7 +61,8 @@ int CalcDueDays(Date year1, Date year2){
                 due_days = year2.day - g_month_arr[leap1][year1.day -1] - year1.day -1;
             
             int i = month_difference;
-            while (i != 1 && i != -1){
+            
+            while (i != 1 && i != -1){ // adds 
                 if (i > 0){
                     due_days += g_month_arr[leap2][year2.month - 1 + i];
                     i--;

5007ac05c256aead3056cb91a36d74fe2ed47488
Dawg4321 <ryan.wiebe@outlook.com>
2021-09-27 21:33:09 +0100

    Flow Control Improvements

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #include "date.c"
 
+void DisplayAssignments();
 
 int main(int argc, char **argv){
 // *** Processing CLI Options ***
@@ -14,13 +15,20 @@ int main(int argc, char **argv){
     if (argv[1][0] == '-' && strlen(argv[1])==2){ // check if CLI option is correct
         switch(argv[1][1]){
             // ** Options **
-            case 'l': // list assignments opt
-                lflag = 1;
-                break;
-            case 'a': // add assignment opt
+            case 'l': // list assignments option
+
+                if (argc >= 3){ // argc >= means that input with -l option has args. This is incorrect behaviour as -l requires no args 
+                    printf("shouldn't have args with l"); // TODO: Implement proper error message for -l with largs
+                    return -1;
+                }
+                else{
+                    lflag = 1;
+                    break;
+                }
+            case 'a': // add assignment option
                 aflag = 1;
                 break;
-            case 'r': // remove assignment opt
+            case 'r': // remove assignment option
                 rflag = 1;
                 break;
             case 'h': // help option
@@ -28,45 +36,48 @@ int main(int argc, char **argv){
                 break;
             default:
                 printf("Error message"); // TODO: Create a propr output message regarding incorrect option
-                return 1; 
+                return -1; 
         }
     }
-    
-    // *** Parsing CLI Arguments ***
-    // Remove [program] [option] from argv to before processing args
-    int len_args = argc - 1;
-    char **args = malloc(len_args); // array to hold args 
-    
-    for (int i = 1; i < argc; i++){ // Copying args into new array (args = argv[2 -> (argc-1)])
-        args[i-1] = malloc(sizeof(argv[i]));
-        strcpy(args[i-1], argv[i]);
-    }
+
+    if(lflag != 1){
+        // *** Parsing CLI Arguments ***
+        // Remove [program] [option] from argv to before processing args
+        int len_args = argc - 1;
+        char **args = malloc(len_args); // array to hold args 
+        
+        for (int i = 1; i < argc; i++){ // Copying args into new array (args = argv[2 -> (argc-1)])
+            args[i-1] = malloc(sizeof(argv[i]));
+            strcpy(args[i-1], argv[i]);
+        }
 
 
-    int opt, dflag = 0, cflag = 0, nflag = 0; // CLI argument flags
-    char *cvalue, *dvalue, *nvalue; // CLI argument values
+        int opt, dflag = 0, cflag = 0, nflag = 0; // CLI argument flags
+        char *cvalue, *dvalue, *nvalue; // CLI argument values
 
-    opterr = 0; // disable getopt() error printout
+        opterr = 0; // disable getopt() error printout
 
-    while((opt = getopt(len_args,args,"d:n:c:r:i:")) != -1){ // loop through each arg 
-        switch(opt){
-            // ** Arguments **
-            case 'd': // due date arg
-                dflag = 1;
-                dvalue = optarg;
-                break;
-            case 'c': // class name arg
-                cflag = 1;
-                cvalue = optarg;
-                break;
-            case 'n': // assignment name arg
-                nflag = 1;
-                nvalue = optarg;
-                break;
-            case '?':
-                // TODO: Implement ? case
-                break;
+        while((opt = getopt(len_args,args,"d:n:c:")) != -1){ // loop through each arg 
+            switch(opt){
+                // ** Arguments **
+                case 'd': // due date arg
+                    dflag = 1;
+                    dvalue = optarg;
+                    break;
+                case 'c': // class name arg
+                    cflag = 1;
+                    cvalue = optarg;
+                    break;
+                case 'n': // assignment name arg
+                    nflag = 1;
+                    nvalue = optarg;
+                    break;
+                case '?':
+                    // TODO: Implement ? case
+                    break;
+            }
         }
     }
+
     return 0;
 }
